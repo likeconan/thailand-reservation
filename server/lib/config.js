531d058c5f7ad94eb2be10c0f5bda45121ed355d
likeconan <liyijia@126.com>
2017-06-12 22:44:44 +0800

    server config

--- server/lib/config.js
+++ server/lib/config.js
@@ -3,5 +3,10 @@ module.exports = {
         port: 3030,
         db_connect: 'mongodb://localhost:27017/Thailand',
         secretKey: 'thailand'
+    },
+    production: {
+        port: 3030,
+        db_connect: 'mongodb://localhost:27017/Thailand',
+        secretKey: 'thailand'
     }
 }
